0b3e9c8545ad0154a278e96a597a27c6c78ea500
prashantkumarbhalke <68597756+prashantkumarbhalke@users.noreply.github.com>
2020-07-23 07:14:17 +0530

    Add files via upload

---
+++ n_num.py
@@ -0,0 +1,7 @@
+end_digit=int(input('enter end digit:'))
+sum=0
+start_value=1
+while start_value<=end_digit:
+    sum=sum+start_value
+    start_value=start_value+1
+print("sum of n natural no is: ",sum)
